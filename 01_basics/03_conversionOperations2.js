20cbb1c18a7171ef942c3d8137a3161a4877999d
Tavish-28 <156124086+Tavish-28@users.noreply.github.com>
2024-08-28 12:14:04 +0000

    conversion on operational data types

---
+++ 01_basics/03_conversionOperations2.js
@@ -0,0 +1,18 @@
+let score="22bn";
+
+console.log(typeof score)
+console.log(typeof (score));
+let hello =Number(score);
+
+console.log(typeof hello);
+console.log(hello);
+
+let hi="hitesh"
+let hmm=Boolean(typeof hi)
+console.log(hmm)
+console.log(hi)
+
+let tavish=33
+console.log(typeof tavish)
+let hey=String(tavish);
+console.log(tavish)
---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,8 @@
+let score="22bn";
+
+console.log(typeof score)
+console.log(typeof (score));
+let hello =Number(score);
+
+console.log(typeof hello);
+console.log(hello);
